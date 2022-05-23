8cbd8423a992211efd85ae2ef33b39912a897527
acer <acer@DESKTOP-8QT4ON4>
2022-05-23 12:47:55 +0530

    new1

---
+++ src/main/java/Scripts/test3.java
@@ -0,0 +1,15 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test3 {
+	@Test
+	public void t4() {
+		
+		Reporter.log("testing ",true);
+		
+		
+	}
+
+}
